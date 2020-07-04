547057d3f123d22255e3a77bda28616f6071e5a8
github username <github email address>
2020-07-03 21:40:58 -0400

    fixed API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name:"Greg",about:"I teach code and like beef",age:30,interest:"Beef,Beef and lots of Beef",location:'New York, NY', img_url:"https://ca.slack-edge.com/T02MD9XTF-U8H2RA3C1-643c8ce562ef-512")
+User.destroy_all
+Song.destroy_all
+ user1 = User.create!(name:"Greg",about:"I teach code and like beef",age:30,interest:"Beef,Beef and lots of Beef",location:'New York, NY', img_url:"https://ca.slack-edge.com/T02MD9XTF-U8H2RA3C1-643c8ce562ef-512")
 
+Song.create!(user_id:user1.id, title:'3000 miles')
 
